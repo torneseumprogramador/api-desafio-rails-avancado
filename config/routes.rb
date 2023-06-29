ebd130e81dac1fe77e0007ad72bd2eab98072171
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-06-29 06:36:34 -0300

    trabalhando com rotas encadeadas e modulos

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :clientes
+
+  namespace :v1 do
+    get "/", to: "v1#index"
+    resources :clientes
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
